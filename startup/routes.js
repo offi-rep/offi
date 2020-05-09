b1ab46006e6383822a8fc5bab4c945e7c185c778
uriportnoy <uriportnoy@gmail.com>
2020-05-09 12:34:56 +0300

    enable cors

--- startup/routes.js
+++ startup/routes.js
@@ -2,8 +2,10 @@ const users = require('../API/usersApi');
 const messages = require('../API/messages');
 const potentials = require('../API/potentials');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 module.exports = (app) => {
+   app.use(cors());
    app.use(bodyParser.json());
    app.use('/users', users);
    app.use('/messages', messages);
