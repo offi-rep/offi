01eef7c3475e3ca45a406347aeb6680f63f9f792
Yoav Keren <yoavke@gmail.com>
2020-06-05 18:27:43 +0300

    added messages

--- API/messages.js
+++ API/messages.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const pgPool = require('../startup/db');
+const logger = require('../startup/logging');
 
 const {getMessages, addMessage} = require('../data/msgs');
 
-router.get('/:personOne/:personTwo', (req,res) => {
-    const person1 = req.params.personOne;
-    const person2 = req.params.personTwo;
+router.get('/:secondUserId', async (req,res) => {
+    const userId = req.header('userId');
+    const secondUserId = req.params.secondUserId;
 
-    const conversation = getMessages(person1,person2);
+    logger.info(`user (${userId}) is looking for his chat with user (${secondUserId})`);
+    const query = {
+        text: "SELECT * FROM messages WHERE (from_user_id=$1 AND to_user_id=$2) OR (from_user_id=$2 AND to_user_id=$1) ORDER BY time_sent DESC",
+        values: [userId, secondUserId]
+    }
 
-    res.status(200).send(JSON.stringify({result: 'Success', data: conversation}));
+    const queryResult = await pgPool.query(query);
+    logger.info(`finished searching. ${JSON.stringify(queryResult.rows)}`);
+
+    res.status(200).send(JSON.stringify({result: 'Success', data: queryResult.rows}));
 });
 
-router.post('/', (req,res) => {
-    const {idFrom, idTo, value} = req.body;
-    const message = {
-        timestamp: Date.now(),
-        idFrom,
-        idTo,
-        value,
-        isRead: false
-    };
+router.post('/', async (req,res) => {
+    const userId = req.header('userId');
+    const {secondUserId,message} = req.body;
 
-    addMessage(message);
-    const messages = getMessages(idFrom, idTo);
+    const querySendMessage = {
+        text: "INSERT INTO messages(from_user_id,to_user_id,message) SELECT $1,$2,$3",
+        values: [userId, secondUserId, message]
+    }
 
-    res.status(201).send(JSON.stringify({result: 'Success', data: messages}));
+    const queryUpdateLastMessage = {
+        text: "UPDATE matches SET last_message=$1 WHERE (first_user_id=$2 AND second_user_id=$3) OR (first_user_id=$3 AND second_user_id=$2)",
+        values: [message, userId, secondUserId]
+    }
+
+    try {
+        await pgPool.query(querySendMessage);
+        await pgPool.query(queryUpdateLastMessage);
+        return res.status(201).send(JSON.stringify({result: 'Success', data: 'Message sent'}));
+    } catch (ex) {
+        logger.error(`couldnt post new message. ${JSON.stringify(req.body)} \n error msg: ${ex.message}`);
+        return res.status(400).send(JSON.stringify({result: 'Failed', data: {msg: ex.message}}))
+    }
 })
 
 module.exports = router;
